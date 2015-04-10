f3e66f65c5dc0a210cdac3df6c8c43ebc73ac96d
Philip Peterson <im@starryexpanse.com>
2015-04-09 22:10:59 -0400

    Switched over to a more direct translation of the MATLAB code so that we can get this up and running in UE4 as soon as possible.

--- Experiment/math.cpp
+++ Experiment/math.cpp
@@ -47,139 +47,73 @@ void printvec(double* H, int n) {
    cout << endl;
 }
 
+inline double sq(double a) {
+   return a*a;
+}
+
 inline void firstHalfStep(
    int n,
+   double g,
    double dt,
    double dx,
+   double dy,
+
    double *H,
-   double *Hx,
    double *U,
+   double *V,
+   double *Hx,
    double *Ux,
    double *Vx,
    double *Hy,
    double *Uy,
-   double *Vy,
-   double *swap
+   double *Vy
+
 ) {
 
-   H[0]=.1;
-   H[1]=.2;
-   H[2]=.3;
-   H[3]=.4;
-
-   H[4]=.5;
-   H[5]=.6;
-   H[6]=.7;
-   H[7]=.8;
-
-   H[8]=.9;
-   H[9]=.109;
-   H[10]=.119;
-   H[11]=.129;
-
-   H[12]=.13;
-   H[13]=.14;
-   H[14]=.15;
-   H[15]=.16;
-
-   cout << "H:" << endl;
-   printmat(H, n+2);
-   cout << endl << "Hx:" << endl;
-   printmat(Hx, n+1);
-   cout << endl << "U:" << endl;
-   printmat(U, n+2);
-   cout << endl;
    
-   // Original matlab source:
-   //    <1>
-   //    Hx(i,j) = (H(i+1,j+1)+H(i,j+1))/2 - dt/(2*dx)*(U(i+1,j+1)-U(i,j+1));
-   // Break that down into:
-   //    <1.a> Hx(i,j) = (H(i+1,j+1)+H(i,j+1))/2)
-   //    <1.b> Hx(i,j) = Hx(i,j) - dt/(2*dx)*(U(i+1,j+1)-U(i,j+1));
-   // Break <1.a> into
-   //    <1.a.1> Hx(i,j) = H(i+1,j+1)+H(i,j+1)
-   //    <1.a.2> Hx(i,j) = Hx(i,j)/2
-   // And since i = 1:n+1 and j = 1:n, 
-   //    <1.a.1> Hx([1:n+1],[1:n]) = H([2:n+2],[2:n+1])+H([1:n+1],[2:n+1])
-   //    <1.a.2> Hx([1:n+1],[1:n]) = Hx(i,j)/2
-
+   // x direction
    for (int j = 0; j < n; j++) {
-      // Height
-      //    <1.a.1> Hx([1:n+1],[1:n]) = H([2:n+2],[2:n+1])+H([1:n+1],[2:n+1])
-      cblas_dcopy(
-         n+1,
-         (const double*)(H+n+2+j+1),
-         n+2,
-         Hx+j,
-         n+1
-      );
-      cblas_daxpy(
-         n+1,
-         1,
-         (const double*)(H+1+j),
-         n+1,
-         Hx+j,
-         n+1
-      );
-      //    <1.a.2> Hx([1:n+1],[1:n]) = Hx(i,j)/2
-      cblas_dscal(
-         n+1,
-         .5,
-         Hx+j,
-         n+1
-      );
-      // Since i = 1:n+1 and j = 1:n, 
-      //    <1.b> Hx(i,j) = Hx(i,j) - dt/(2*dx)*(U(i+1,j+1)-U(i,j+1));
-      //    <1.b> Hx([1:n+1],[1:n]) = Hx([1:n+1],[1:n]) - dt/(2*dx)*(U(i+1,j+1)-U(i,j+1));
-      //    <1.b> Hx([1:n+1],[1:n]) += - dt/(2*dx)*(U(i+1,j+1)-U(i,j+1));
-
-      // In other words:
-      //    <1.b.1> swap = U([2:n+2],[2:n+1]) ;
-      cblas_dcopy(
-         n+1,
-         (const double*)(U+(n+2)+j+1),
-         n+2,
-         swap,
-         1
-      );
-      cout << "Swap is " <<endl;
-      printvec(swap, n+1);
-      //    <1.b.2> swap = -U([1:n+1],[2:n+1]) + swap ;
-      cblas_daxpy(
-         n+1,
-         -1,
-         (const double*)(U+j+1),
-         n+2,
-         swap,
-         1
-      );
-      //    <1.b.3> Hx([1:n+1],[1:n]) += - dt/(2*dx)*swap;
-      cblas_daxpy(
-         n+1,
-         -dt/(2*dx),
-         (const double*)(swap),
-         1,
-         Hx,
-         n+1
-      );
-
-      printmat(Hx, n+1);
-
-      // Original matlab source:
-      //    <2>
-      //    Ux(i,j) = (U(i+1,j+1)+U(i,j+1))/2 -  ...
-      //                dt/(2*dx)*((U(i+1,j+1).^2./H(i+1,j+1) + g/2*H(i+1,j+1).^2) - ...
-      //                (U(i,j+1).^2./H(i,j+1) + g/2*H(i,j+1).^2));
-      //    Ux(i,j) = (
-      //                U(i+1,j+1)+U(i,j+1)
-      //               )/2 
-      //               -  dt/(2*dx)*(
-      //                   (U(i+1,j+1).^2./H(i+1,j+1) + g/2*H(i+1,j+1).^2) - ...
-      //                   (U(i,j+1).^2./H(i,j+1) + g/2*H(i,j+1).^2)
-      //                );
+      for (int i = 0; i < n+1; i++) {
+         
+       // height
+       Hx[i*(n+1)+j] = 
+         (H[(i+1)*(n+2)+j+1]+H[i*(n+2)+j+1])/2
+         - dt/(2*dx)*(U[(i+1)*(n+2)+j+1]-U[i*(n+2)+j+1]);
    
+       // x momentum
+       Ux[i*(n+1),j] = 
+            (U[(i+1)*(n+2),j+1]+U[i*(n+2),j+1])/2 - 
+            dt/(2*dx)*(
+                  (
+                     sq(U[(i+1)*(n+2)+j+1])/H[(i+1)*(n+2),j+1] 
+                     + g/2*sq(H[(i+1)*(n+2)+j+1])
+                  ) - (
+                     sq(U[i*(n+2)+j+1])/H[i*(n+2)+j+1] + g/2*sq(H[i*(n+2)+j+1])
+                  )
+            );
+   
+       //// y momentum
+       Vx[i,j] = (V[i+1,j+1]+V[i,j+1])/2 - 
+                 dt/(2*dx)*((U[(n+2)*(i+1)+j+1]*V[(i+1)*(n+2)+j+1]/H[(i+1)*(n+2)+j+1]) -
+                            (U[(n+2)*(i)+j+1]*V[i*(n+2)+j+1]/H[i*(n+2)+j+1]));
+      }
+   }
 
-      
+   // y direction
+   for (int j = 0; j < n+1; j++) {
+      for (int i = 0; i < n; i++) {
+       // height
+       Hy[i,j] = (H[i+1,j+1]+H[i+1,j])/2 - dt/(2*dy)*(V[i+1,j+1]-V[i+1,j]);
+   
+       // x momentum
+       Uy[i,j] = (U[(i+1)*(n+2)+j+1]+U[(i+1)*(n+2)+j])/2 -
+                 dt/(2*dy)*((V[(i+1)*(n+2)+j+1]*U[(i+1)*(n+2)+j+1]/H[(i+1)*(n+2)+j+1]) -
+                            (V[(i+1)*(n+2)+j]*U[(i+1)*(n+2)+j]/H[(i+1)*(n+2)+j]));
+       // y momentum
+       Vy[i,j] = (V[(i+1)*(n+2)+j+1]+V[(i+1)*(n+2)+j])/2 -
+                 dt/(2*dy)*((sq(V[(i+1)*(n+2)+j+1])/H[(i+1)*(n+2)+j+1] + g/2*sq(H[(n+2)*(i+1)+j+1])) -
+                            (sq(V[(i+1)*(n+2)+j])/H[(i+1)*(n+2)+j] + g/2*sq(H[(i+1)*(n+2)+j])));
+      }
    }
    cout << endl;
 }
@@ -188,7 +122,8 @@ inline void firstHalfStep(
 int main(int argc, char** argv) {
    int n = 64;
    double dt = 0.1;
-   double dx = 0.1;
+   double dx = 1.0;
+   double dy = 1.0;
 
    n = 2; // testing
 
@@ -217,8 +152,7 @@ int main(int argc, char** argv) {
 
    double *swap = (double*)malloc(sizeof(*swap) * (n+1));
 
-   firstHalfStep(n, dt, dx, H, Hx, U, Ux, Vx, Hy, Uy, Vy, swap);
-
+   firstHalfStep(n, -9.8, dt, dx, dy, H, U, V, Hx, Ux, Vx, Hy, Uy, Vy);
 
    return 0;
 }
